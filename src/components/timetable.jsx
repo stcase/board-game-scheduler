2e7a399e9fc231fa82348d97a829be01b054d12c
Steven Case <stcinc@gmail.com>
2021-09-17 15:46:25 +0200

    Fix modulo bug
    mod of a negative shouldn't return a negative number

--- src/components/timetable.jsx
+++ src/components/timetable.jsx
@@ -7,7 +7,7 @@ class Timetable extends React.Component {
       <tr>
         <td>{ttable.name}</td>
         {[...Array(24).keys()].map((hour) => {
-          const adjustedHour = (hour + ttable.gmtOffset) % 24;
+          const adjustedHour = mod(hour + ttable.gmtOffset, 24);
           return (
             <td
               key={ttable.id + "-" + adjustedHour}
@@ -33,6 +33,11 @@ class Timetable extends React.Component {
       </tr>
     );
   }
+}
+
+function mod(n, m) {
+  // Fix the mod bug with negative numbers
+  return ((n % m) + m) % m;
 }
 
 export default Timetable;
